5d226aca7152f336fb55eae268b6808bfc6b0f33
김민태 <kimmintae981019@gmail.com>
2023-09-07 17:40:06 +0900

    Fix: 마이페이지 Feed 목록 조회 수정

--- backend/vh/src/main/java/com/it/vh/feed/service/FeedService.java
+++ backend/vh/src/main/java/com/it/vh/feed/service/FeedService.java
@@ -5,5 +5,5 @@ import com.it.vh.feed.api.dto.FeedResDto;
 import java.util.List;
 
 public interface FeedService {
-    List<FeedResDto> getFeedsByUserId(long userId, boolean isMe, int searchType, String keyword, int page);
+    List<FeedResDto> getFeedsByUserId(long userId, long myId, int searchType, String keyword, int page);
 }
