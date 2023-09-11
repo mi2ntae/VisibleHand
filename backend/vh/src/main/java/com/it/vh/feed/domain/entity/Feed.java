66489a36f77539db38833af2fffb621c5b6fe96e
김민태 <kimmintae981019@gmail.com>
2023-09-11 12:27:19 +0900

    Feat: 피드 목록 조회 쿼리 작성

--- backend/vh/src/main/java/com/it/vh/feed/domain/entity/Feed.java
+++ backend/vh/src/main/java/com/it/vh/feed/domain/entity/Feed.java
@@ -44,6 +44,35 @@ import java.time.LocalDateTime;
                         "       on F.article_id = A.article_id " +
                         "       where F.content like :keyword and F.share >= :isOther and F.user_id = :userId " +
                         "       order by createAt desc",
+
+                resultSetMapping = "findFeedsAndHeartAndIsHeartByUserIdWhereTitleIsKeyword"
+        ),
+        @NamedNativeQuery(
+                name = "findFeedsByFollowingUserIdAndContent",
+                query = "SELECT F.feed_id as feedId, F.content, A.article_id as articleId, A.title, F.create_at as createAt, " +
+                        "       IF((select count(*) from Heart " +
+                        "       where Heart.user_id = :userId and Heart.feed_id = F.feed_id) <= 0, false, true) as isHeart, " +
+                        "       (select count(*) from Heart where Heart.feed_id = F.feed_id) as heart " +
+                        "       from Feed as F " +
+                        "       join Article as A " +
+                        "       on F.article_id = A.article_id " +
+                        "       join Follow as FF on F.user_id = FF.to_id " +
+                        "       where F.content like :keyword and F.share >= 1 and FF.from_id = :userId " +
+                        "       order by createAt desc",
+                resultSetMapping = "findFeedsAndHeartAndIsHeartByUserIdWhereTitleIsKeyword"
+        ),
+        @NamedNativeQuery(
+                name = "findFeedsByFollowingUserIdAndTitle",
+                query = "SELECT F.feed_id as feedId, F.content, A.article_id as articleId, A.title, F.create_at as createAt, " +
+                        "       IF((select count(*) from Heart " +
+                        "       where Heart.user_id = :userId and Heart.feed_id = F.feed_id) <= 0, false, true) as isHeart, " +
+                        "       (select count(*) from Heart where Heart.feed_id = F.feed_id) as heart " +
+                        "       from Feed as F " +
+                        "       join Article as A " +
+                        "       on F.article_id = A.article_id " +
+                        "       join Follow as FF on F.user_id = FF.to_id " +
+                        "       where A.title like :keyword and F.share >= 1 and FF.from_id = :userId " +
+                        "       order by createAt desc",
                 resultSetMapping = "findFeedsAndHeartAndIsHeartByUserIdWhereTitleIsKeyword"
         ),
 })
