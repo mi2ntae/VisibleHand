2feedc56d7a551f45efa4654100c9dad32c66800
Seulgi <urina842@gmail.com>
2023-09-12 17:54:33 +0900

    Refactor: JwtTokenProvider 수정 중

--- backend/vh/src/main/java/com/it/vh/common/util/JwtTokenProvider.java
+++ backend/vh/src/main/java/com/it/vh/common/util/JwtTokenProvider.java
@@ -1,15 +1,21 @@
 package com.it.vh.common.util;
 
+import com.it.vh.user.domain.entity.User;
+import com.it.vh.user.domain.repository.UserRespository;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import java.util.Date;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
 @Slf4j
+@RequiredArgsConstructor
 @Component
 public class JwtTokenProvider {
 
@@ -20,14 +26,15 @@ public class JwtTokenProvider {
     @Value("${jwt.refresh-expired-in}")
     private Long refreshToken;
 
+    private final UserRespository userRepository;
+
     public String createAccessToken(Long userId) {
-        Claims claims = Jwts.claims();
-        claims.put("user_role", "ROLE_USER");
-        claims.put("userId", userId);
+//        Claims claims = Jwts.claims();
+//        claims.setSubject(String.valueOf(userId));
 
         return Jwts.builder()
-            .setHeaderParam("type", "accessToken")
-            .setClaims(claims)
+            .setSubject(String.valueOf(userId))
+            .claim("userId", userId)
             .setIssuedAt(new Date(System.currentTimeMillis()))
             .setExpiration(new Date(System.currentTimeMillis() + accessToken))
             .signWith(SignatureAlgorithm.HS384, secretKey)
@@ -35,16 +42,40 @@ public class JwtTokenProvider {
     }
 
     public String createRefreshToken(Long userId) {
-        Claims claims = Jwts.claims();
-        claims.put("userId", userId);
-
         return Jwts.builder()
-            .setHeaderParam("type", "refreshToken")
-            .setClaims(claims)
+            .setSubject(String.valueOf(userId) + "_refresh")
+            .claim("userId", userId)
             .setIssuedAt(new Date(System.currentTimeMillis()))
-            .setExpiration(new Date(System.currentTimeMillis() + accessToken))
+            .setExpiration(new Date(System.currentTimeMillis() + refreshToken))
             .signWith(SignatureAlgorithm.HS384, secretKey)
             .compact();
+    }
+
+    //Authentication 생성
+    public Authentication getAuthentication(String token) {
+        Claims claims = parseClaims(token);
+
+        if (claims.get("userId") == null) {
+            return null;
+        }
+
+        Long userId = Long.valueOf(claims.get("userId").toString());
+        log.info("userId: {}", userId);
+
+        Optional<User> findUser = userRepository.findById(userId);
+        if (findUser.isEmpty()) {
+            return null;
+        }
+
+        return null;
+    }
+
+    private Claims parseClaims(String token) {
+        return Jwts.parserBuilder()
+            .setSigningKey(secretKey)
+            .build()
+            .parseClaimsJws(token)
+            .getBody();
     }
 
     public boolean isValidateToken(String token) {
@@ -58,11 +89,11 @@ public class JwtTokenProvider {
         }
     }
 
-    public Long getUserId(String token) {
-        return Jwts.parser()
-            .setSigningKey(secretKey)
-            .parseClaimsJws(token)
-            .getBody()
-            .get("userId", Long.class);
-    }
+//    public Long getUserId(String token) {
+//        return Jwts.parser()
+//            .setSigningKey(secretKey)
+//            .parseClaimsJws(token)
+//            .getBody()
+//            .get("userId", Long.class);
+//    }
 }
