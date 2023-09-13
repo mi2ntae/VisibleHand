56b7671c8c66330eb0f4dbf934bab9eab09d4c84
lojo0307 <lojo0307@naver.com>
2023-09-13 16:17:28 +0900

    Feat: 컴포넌트 및 컬러팔레트 생성

--- frontend/src/components/user/mypage/down/tabs/MyPageTab.jsx
+++ frontend/src/components/user/mypage/down/tabs/MyPageTab.jsx
@@ -1,18 +1,28 @@
-import React from 'react';
+import React from "react";
 import "../css/Tab.css";
-import { useDispatch } from 'react-redux';
-import { setTabNo } from '../../../../../reducer/mypageTabReducer';
+import { useDispatch } from "react-redux";
+import { setTabNo } from "../../../../../reducer/mypageTabReducer";
 
-export default function MyPageTab({no, tabName, isFirst}) {
-    const dispatch = useDispatch();
+export default function MyPageTab({ no, tabName, isFirst }) {
+  const dispatch = useDispatch();
 
-    const handleChange = (e) => {
-        dispatch(setTabNo(parseInt(e.currentTarget.value)));
-    }
-    return (
-        <label class="radio">
-            {isFirst ? <input type="radio" name="radio" value={no} defaultChecked onChange={handleChange}/> : <input type="radio" name="radio" value={no} onChange={handleChange}/>}
-            <span class="name">{tabName}</span>
-        </label>
-    );
+  const handleChange = (e) => {
+    dispatch(setTabNo(parseInt(e.currentTarget.value)));
+  };
+  return (
+    <label class="radio">
+      {isFirst ? (
+        <input
+          type="radio"
+          name="radio"
+          value={no}
+          defaultChecked
+          onChange={handleChange}
+        />
+      ) : (
+        <input type="radio" name="radio" value={no} onChange={handleChange} />
+      )}
+      <span class="name">{tabName}</span>
+    </label>
+  );
 }
