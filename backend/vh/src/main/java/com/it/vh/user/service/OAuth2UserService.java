0aa10c31facb85bc8d6483a403e2d513fdb601a0
Seulgi <urina842@gmail.com>
2023-09-13 17:53:06 +0900

    Feat: 권한 설정 확인

--- backend/vh/src/main/java/com/it/vh/user/service/OAuth2UserService.java
+++ backend/vh/src/main/java/com/it/vh/user/service/OAuth2UserService.java
@@ -1,6 +1,7 @@
 package com.it.vh.user.service;
 
-import com.it.vh.common.util.JwtTokenProvider;
+import com.it.vh.common.util.jwt.CustomUserDetails;
+import com.it.vh.common.util.jwt.JwtTokenProvider;
 import com.it.vh.user.api.dto.auth.KakaoUserInfo;
 import com.it.vh.user.api.dto.auth.LoginResDto;
 import com.it.vh.user.api.dto.auth.LoginResDto.TokenInfo;
@@ -10,6 +11,7 @@ import com.it.vh.user.api.dto.auth.OAuth2UserInfo;
 import com.it.vh.user.domain.dto.UserDto;
 import com.it.vh.user.domain.entity.User;
 import com.it.vh.user.domain.repository.UserRespository;
+import io.jsonwebtoken.Header;
 import java.nio.charset.StandardCharsets;
 import java.util.Collections;
 import java.util.Map;
@@ -18,8 +20,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.MediaType;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 import org.springframework.stereotype.Service;
@@ -37,6 +42,7 @@ public class OAuth2UserService {
     private final InMemoryClientRegistrationRepository inMemoryRepository;
     private final UserRespository userRespository;
     private final JwtTokenProvider jwtTokenProvider;
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
 
     @Transactional
     public LoginResDto login(String code) {
@@ -45,37 +51,88 @@ public class OAuth2UserService {
         //토큰 받기
         OAuth2TokenInfo token = getToken(code, provider);
 
-        //일단은 DTO로 가지고 있다가 프로필 작성 후 가입 또는 로그인 처리
-        UserDto userDto = getUser(token, provider);
-        log.info("userId: {}", userDto.getUserId());
+        //가입 또는 로그인 처리
+        User user = getUser2(token, provider);
+        log.info("user: {}", user);
 
         UserProfile userProfile = UserProfile.builder()
-            .userId(userDto.getUserId())
-            .nickname(userDto.getNickname())
-            .statusMsg(userDto.getStatusMsg())
-            .profileImg(userDto.getProfileImg())
-            .snsEmail(userDto.getSnsEmail())
+            .userId(user.getUserId())
+            .nickname(user.getNickname())
+            .statusMsg(user.getStatusMsg())
+            .profileImg(user.getProfileImg())
+            .snsEmail(user.getSnsEmail())
             .build();
 
-        //하지만 이미 가입된 회원은
-        //로그인 시 jwt 토큰 발급
-        if(userDto.getUserId()!=null) {
-            String accessToken = jwtTokenProvider.createAccessToken(userDto.getUserId());
-            String refreshToken = jwtTokenProvider.createRefreshToken(userDto.getUserId());
+        //jwt 토큰 발급
+        String accessToken = jwtTokenProvider.createAccessToken(user.getUserId());
+        String refreshToken = jwtTokenProvider.createRefreshToken(user.getUserId());
 
-            TokenInfo tokenInfo = TokenInfo.builder()
+        TokenInfo tokenInfo = TokenInfo.builder()
             .accessToken(accessToken)
             .refreshToken(refreshToken)
             .build();
 
-            return LoginResDto.builder()
-                .user(userProfile)
-                .token(tokenInfo)
-                .build();
-        }
+        log.info("-----------------------권한 확인 시작");
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        log.info("등록 전 auth: {}", auth);
+
+        UserDetails userDetails = new CustomUserDetails(user);
+        UsernamePasswordAuthenticationToken authenticationToken
+            = new UsernamePasswordAuthenticationToken(userDetails, "",
+            userDetails.getAuthorities());
+
+        Authentication authentication = jwtTokenProvider.getAuthentication(accessToken);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        Authentication auth2 = SecurityContextHolder.getContext().getAuthentication();
+        log.info("등록 후 auth: {}", auth2);
+
+        Long num = Long.parseLong(auth2.getName());
+        log.info("userId 확인: {}", num);
+        log.info("권한 확인 끝-----------------------");
+
+        Header header = jwtTokenProvider.parseHeader(accessToken);
+        Header header2 = jwtTokenProvider.parseHeader(refreshToken);
+        log.info("header: {}", header);
+        log.info("header2: {}", header2);
+
+        //-------------------------------------------------
+        //일단은 DTO로 가지고 있다가 프로필 작성 후 가입 또는 로그인 처리
+//        UserDto userDto = getUser(token, provider);
+//        log.info("userId: {}", userDto.getUserId());
+
+//        UserProfile userProfile = UserProfile.builder()
+//            .userId(userDto.getUserId())
+//            .nickname(userDto.getNickname())
+//            .statusMsg(userDto.getStatusMsg())
+//            .profileImg(userDto.getProfileImg())
+//            .snsEmail(userDto.getSnsEmail())
+//            .build();
+
+//        //하지만 이미 가입된 회원은
+//        //로그인 시 jwt 토큰 발급
+//        //근데 일단은 이 주소로 요청 들어오면 다 토큰 발급되도록 해놓음
+//        if (userDto.getUserId() != null) {
+//            String accessToken = jwtTokenProvider.createAccessToken(userDto.getUserId());
+//            String refreshToken = jwtTokenProvider.createRefreshToken(userDto.getUserId());
+//
+//            String accessToken = jwtTokenProvider.createAccessToken(userDto.getUserId());
+//            String refreshToken = jwtTokenProvider.createRefreshToken(userDto.getUserId());
+//
+//            TokenInfo tokenInfo = TokenInfo.builder()
+//                .accessToken(accessToken)
+//                .refreshToken(refreshToken)
+//                .build();
+//
+//            return LoginResDto.builder()
+//                .user(userProfile)
+//                .token(tokenInfo)
+//                .build();
+//        }
 
         return LoginResDto.builder()
             .user(userProfile)
+            .token(tokenInfo)
             .build();
     }
 
@@ -103,7 +160,6 @@ public class OAuth2UserService {
         return formData;
     }
 
-    // TODO: 프로필 이미지, 닉네임 추가 동의 받아서 프로필 설정 페이지로 넘어갈 때 미리 설정되어 있도록
     private UserDto getUser(OAuth2TokenInfo token, ClientRegistration provider) {
         Map<String, Object> userAttributes = getUserAttributes(token, provider);
 
@@ -122,6 +178,23 @@ public class OAuth2UserService {
         return userDto;
     }
 
+    private User getUser2(OAuth2TokenInfo token, ClientRegistration provider) {
+        Map<String, Object> userAttributes = getUserAttributes(token, provider);
+
+        OAuth2UserInfo oAuth2UserInfo = new KakaoUserInfo(userAttributes);
+        String snsEmail = oAuth2UserInfo.getEmail();
+
+        User saveUser = User.builder()
+            .nickname("testNick")
+            .profileImg("testImg")
+            .statusMsg("testMsg")
+            .snsEmail(snsEmail)
+            .build();
+        userRespository.save(saveUser);
+
+        return saveUser;
+    }
+
     private Map<String, Object> getUserAttributes(OAuth2TokenInfo token,
         ClientRegistration provider) {
         return WebClient.create()
@@ -136,4 +209,18 @@ public class OAuth2UserService {
             .block();
     }
 
+//    public void logout(Long userId) {
+//        //accessToken 불러오기
+//        String accessToken = jwtTokenProvider.getAccessToken(userId);
+//
+//       WebClient.ResponseSpec res = WebClient.create()
+//            .post()
+//            .uri("https://kapi.kakao.com/v1/user/logout")
+//            .headers(header -> {
+//               header.setBearerAuth(accessToken);
+//            })
+//           .retrieve();
+//    }
+
+    //탈퇴 시 토큰도 만료돼서 다시 동의항목 처리하도록
 }
