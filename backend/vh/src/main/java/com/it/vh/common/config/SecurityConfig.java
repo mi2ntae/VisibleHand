02ffdb387782bb7a2ab8984787e40037e4c0ecc0
Seulgi <urina842@gmail.com>
2023-09-14 17:53:22 +0900

    Refactor: Cors 설정 추가

--- backend/vh/src/main/java/com/it/vh/common/config/SecurityConfig.java
+++ backend/vh/src/main/java/com/it/vh/common/config/SecurityConfig.java
@@ -1,8 +1,11 @@
 package com.it.vh.common.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.it.vh.common.util.jwt.JwtAuthenticationFilter;
 import com.it.vh.common.util.jwt.JwtTokenProvider;
+import com.it.vh.common.util.jwt.filter.JwtAccessDeniedHandler;
+import com.it.vh.common.util.jwt.filter.JwtAuthenticationEntryPoint;
+import com.it.vh.common.util.jwt.filter.JwtAuthenticationFilter;
+import com.it.vh.user.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -11,6 +14,9 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -18,28 +24,56 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfig {
 
     private final JwtTokenProvider jwtTokenProvider;
+    private final UserService userService;
+    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
     private final ObjectMapper objectMapper;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http
-            .headers()
-            .frameOptions()
-            .sameOrigin().and()
-            .cors().and()
+            .cors().configurationSource(corsConfigurationSource())
+            .and()
             .csrf().disable()
-            .sessionManagement()
-            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+
             .and()
             //TODO: 허용할 주소 추가
             .authorizeRequests()
-//            .anyRequest().authenticated()
+//            .antMatchers("").authenticated()
+            .regexMatchers("/api/user/\\d+").authenticated()
             .anyRequest().permitAll()
+
+            .and()
+            .exceptionHandling()
+            .accessDeniedHandler(new JwtAccessDeniedHandler(objectMapper))
+            .authenticationEntryPoint(new JwtAuthenticationEntryPoint(objectMapper))
             .and()
             .addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider, objectMapper),
                 UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
+    }
+
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+//    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.addAllowedOriginPattern("*");
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
     }
 }
