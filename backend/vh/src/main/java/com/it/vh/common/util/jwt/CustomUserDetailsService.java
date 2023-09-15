36b00def219f86f91be748a21519026ba8c5a6d9
Seulgi <urina842@gmail.com>
2023-09-15 17:51:45 +0900

    Refactor: CustomUserDetailsService 수정 중

--- backend/vh/src/main/java/com/it/vh/common/util/jwt/CustomUserDetailsService.java
+++ backend/vh/src/main/java/com/it/vh/common/util/jwt/CustomUserDetailsService.java
@@ -1,8 +1,10 @@
 package com.it.vh.common.util.jwt;
 
+import static com.it.vh.common.exception.ExceptionList.NON_EXIST_USER_ID;
+
 import com.it.vh.user.domain.entity.User;
 import com.it.vh.user.domain.repository.UserRespository;
-import java.util.Optional;
+import com.it.vh.user.exception.NonExistUserIdException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -18,14 +20,23 @@ public class CustomUserDetailsService implements UserDetailsService {
     private final UserRespository userRespository;
 
     @Override
-    public UserDetails loadUserByUsername(String snsEmail) throws UsernameNotFoundException {
-        Optional<User> findUser = userRespository.findBySnsEmail(snsEmail);
-        if (findUser.isEmpty()) {
-            return null;
-        }
+    public UserDetails loadUserByUsername(String userId) throws UsernameNotFoundException {
+        log.info("[권한 설정] userId: {}", userId);
 
-        CustomUserDetails userDetails = new CustomUserDetails(findUser.get());
-        log.info("custom: {}", userDetails);
+        User findUser = userRespository.findById(Long.valueOf(userId)).orElseThrow(
+            () -> new NonExistUserIdException(NON_EXIST_USER_ID.getMessage())
+        );
+
+        CustomUserDetails userDetails = new CustomUserDetails(findUser);
+        log.info("userDetails: {}", userDetails);
         return userDetails;
     }
+
+//    private UserDetails createUserDetails(User user) {
+//        return org.springframework.security.core.userdetails.User.builder()
+//            .username(user.getUserId().toString())
+//            .password(passwordEncoder.encode(user.getUserId().toString()))
+//            .roles(user.getNickname())
+//            .build();
+//    }
 }
