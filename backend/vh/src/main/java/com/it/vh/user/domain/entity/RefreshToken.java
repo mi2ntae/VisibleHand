016610bf5125297dff4838be257024c80f846a18
Seulgi <urina842@gmail.com>
2023-09-15 17:54:40 +0900

    Feat: RefreshToken 추가

---
+++ backend/vh/src/main/java/com/it/vh/user/domain/entity/RefreshToken.java
@@ -0,0 +1,30 @@
+package com.it.vh.user.domain.entity;
+
+
+import java.util.concurrent.TimeUnit;
+import javax.persistence.Entity;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+import org.springframework.data.redis.core.TimeToLive;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+//@RedisHash(value = "refresh_token")
+public class RefreshToken {
+
+    @Id
+    private String id;
+
+    private String refreshToken;
+
+    @TimeToLive(unit = TimeUnit.MILLISECONDS)
+    private Long ttl;
+}
