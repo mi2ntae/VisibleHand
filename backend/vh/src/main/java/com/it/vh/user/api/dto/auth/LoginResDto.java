5e4c012a12eed151e560abd3fc6fff153ef0cbda
seul <l.23.53.l2l9@gmail.com>
2023-09-17 02:17:22 +0900

    Refactor: 생성 메서드 추가

--- backend/vh/src/main/java/com/it/vh/user/api/dto/auth/LoginResDto.java
+++ backend/vh/src/main/java/com/it/vh/user/api/dto/auth/LoginResDto.java
@@ -1,5 +1,7 @@
 package com.it.vh.user.api.dto.auth;
 
+import com.it.vh.common.util.jwt.dto.TokenInfo;
+import com.it.vh.user.domain.entity.User;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -19,15 +21,15 @@ public class LoginResDto {
         private String statusMsg;
         private String profileImg;
         private String snsEmail;
-    }
 
-    @Getter
-    @Builder
-    public static class TokenInfo {
-
-        @Builder.Default
-        private String type = "Bearer";
-        private String accessToken;
-        private String refreshToken;
+        public static UserProfile from(User user) {
+            return UserProfile.builder()
+                    .userId(user.getUserId())
+                    .nickname(user.getNickname())
+                    .statusMsg(user.getStatusMsg())
+                    .profileImg(user.getProfileImg())
+                    .snsEmail(user.getSnsEmail())
+                    .build();
+        }
     }
 }
