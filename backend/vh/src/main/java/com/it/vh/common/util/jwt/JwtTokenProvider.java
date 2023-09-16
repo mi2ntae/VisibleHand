05ebb6255c72b9f3ee105b3e0b474d9cc49cc287
seul <l.23.53.l2l9@gmail.com>
2023-09-17 02:14:28 +0900

    Feat: 토큰 권한 설정 추가

--- backend/vh/src/main/java/com/it/vh/common/util/jwt/JwtTokenProvider.java
+++ backend/vh/src/main/java/com/it/vh/common/util/jwt/JwtTokenProvider.java
@@ -1,40 +1,30 @@
 package com.it.vh.common.util.jwt;
 
-import static com.it.vh.common.util.jwt.exception.JwtExceptionList.EXPIRED_TOKEN;
-import static com.it.vh.common.util.jwt.exception.JwtExceptionList.INVALID_TOKEN;
-
-import com.it.vh.common.util.jwt.dto.Token;
-import com.it.vh.user.api.dto.auth.LoginResDto.TokenInfo;
+import com.it.vh.common.util.jwt.dto.TokenInfo;
 import com.it.vh.user.domain.entity.User;
-import com.it.vh.user.domain.repository.UserRespository;
-import com.sun.security.auth.UnixNumericGroupPrincipal;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.ExpiredJwtException;
-import io.jsonwebtoken.Header;
-import io.jsonwebtoken.JwtException;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.MalformedJwtException;
-import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.UnsupportedJwtException;
+import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
-import io.jsonwebtoken.security.SecurityException;
-import io.jsonwebtoken.security.SignatureException;
-import java.security.Key;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Date;
-import java.util.Optional;
-import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.authority.mapping.GrantedAuthoritiesMapper;
+import org.springframework.security.core.authority.mapping.NullAuthoritiesMapper;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Date;
+import java.util.stream.Collectors;
+
+import static com.it.vh.common.util.jwt.exception.JwtExceptionList.EXPIRED_TOKEN;
+import static com.it.vh.common.util.jwt.exception.JwtExceptionList.INVALID_TOKEN;
 
 
 @Slf4j
@@ -47,6 +37,8 @@ public class JwtTokenProvider {
     @Value("${jwt.refresh-expiration}")
     private Long refresh_expiration;
 
+    private GrantedAuthoritiesMapper authoritiesMapper = new NullAuthoritiesMapper();
+
     public JwtTokenProvider(@Value("${jwt.secret}") String secretKey) {
         byte[] keyBytes = Decoders.BASE64.decode(secretKey);
         this.key = Keys.hmacShaKeyFor(keyBytes);
@@ -55,27 +47,27 @@ public class JwtTokenProvider {
     public TokenInfo generateToken(Authentication authentication) {
         String authorities = authentication.getAuthorities().stream()
                 .map(GrantedAuthority::getAuthority)
-                    .collect(Collectors.joining(","));
+                .collect(Collectors.joining(","));
         log.info("authorities: {}", authorities);
         log.info("userId: {}", authentication.getName());
 
         String accessToken = Jwts.builder()
-            .setSubject(String.valueOf(authentication.getName()))
-            .claim("userId", authentication.getName())
-            .claim("authority", authorities)
-            .setIssuedAt(new Date(System.currentTimeMillis()))
-            .setExpiration(new Date(System.currentTimeMillis() + access_expiration))
-            .signWith(key, SignatureAlgorithm.HS256)
-            .compact();
+                .setSubject(String.valueOf(authentication.getName()))
+                .claim("userId", authentication.getName())
+                .claim("authority", authorities)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + access_expiration))
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
 
         String refreshToken = Jwts.builder()
-            .setSubject(String.valueOf(authentication.getName()) + "_refresh")
-            .claim("userId", authentication.getName())
-            .claim("authority", authorities)
-            .setIssuedAt(new Date(System.currentTimeMillis()))
-            .setExpiration(new Date(System.currentTimeMillis() + refresh_expiration))
-            .signWith(key, SignatureAlgorithm.HS256)
-            .compact();
+                .setSubject(String.valueOf(authentication.getName()) + "_refresh")
+                .claim("userId", authentication.getName())
+                .claim("authority", authorities)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + refresh_expiration))
+                .signWith(key, SignatureAlgorithm.HS256)
+                .compact();
 
         return TokenInfo.builder()
                 .type("Bearer")
@@ -83,11 +75,13 @@ public class JwtTokenProvider {
                 .refreshToken(refreshToken)
                 .build();
     }
-//    public String createAccessToken(Long userId) {
+
+//    public String createAccessToken(Long userId, String snsEmail) {
 //        return Jwts.builder()
 //            .setSubject(String.valueOf(userId))
 //            .claim("userId", userId)
 //            .claim("auth", userId)
+//            .claim("email", snsEmail)
 //            .setIssuedAt(new Date(System.currentTimeMillis()))
 //            .setExpiration(new Date(System.currentTimeMillis() + access_expiration))
 //            .signWith(key, SignatureAlgorithm.HS256)
@@ -104,28 +98,34 @@ public class JwtTokenProvider {
 //            .compact();
 //    }
 
+    public Authentication setAuthentication(User user) {
+        String password = PasswordUtil.generateRandomPassword();
+
+        UserDetails userDetailsUser = org.springframework.security.core.userdetails.User.builder()
+                .username(user.getUserId().toString())
+                .password(password)
+                .roles("USER")
+                .build();
+
+        Authentication authentication =
+                new UsernamePasswordAuthenticationToken(userDetailsUser, "",
+                        authoritiesMapper.mapAuthorities(userDetailsUser.getAuthorities()));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        return authentication;
+    }
+
     public Authentication getAuthentication(String token) {
         Claims claims = parseClaims(token);
 
         Collection<? extends GrantedAuthority> authorities
-            = Arrays.stream(claims.get("auth").toString()
-                .split(",")).map(SimpleGrantedAuthority::new)
+                = Arrays.stream(claims.get("authority").toString()
+                        .split(",")).map(SimpleGrantedAuthority::new)
                 .collect(Collectors.toList());
 
         org.springframework.security.core.userdetails.User user
-            = new org.springframework.security.core.userdetails.User(claims.getSubject(), "", authorities);
+                = new org.springframework.security.core.userdetails.User(claims.getSubject(), "", authorities);
 
-//        Long userId = getUserId(token);
-//        log.info("userId: {}", userId);
-//
-//        Optional<User> findUser = userRepository.findById(userId);
-//        if (findUser.isEmpty()) {
-//            return null;
-//        }
-//        UserDetails userDetails = new CustomUserDetails(findUser.get());
-//        log.info("userDetails: {}", userDetails);
-//        return new UsernamePasswordAuthenticationToken(userDetails, token,
-//            userDetails.getAuthorities());
         return new UsernamePasswordAuthenticationToken(user, token, authorities);
     }
 
@@ -133,11 +133,11 @@ public class JwtTokenProvider {
         Claims claims = null;
         try {
             claims
-                = Jwts.parserBuilder()
-                .setSigningKey(key)
-                .build()
-                .parseClaimsJws(token)
-                .getBody();
+                    = Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(token)
+                    .getBody();
             return claims;
         } catch (ExpiredJwtException e) {
             throw new ExpiredJwtException(parseHeader(token), claims, EXPIRED_TOKEN.getMessage());
@@ -155,25 +155,42 @@ public class JwtTokenProvider {
         return userId;
     }
 
+    public String getUserEmail(String token) {
+        Claims claims = parseClaims(token);
+
+        if (claims.get("email") == null) {
+            return null;
+        }
+
+        String snsEmail = claims.get("email").toString();
+        return snsEmail;
+    }
+
     private Header parseHeader(String token) {
         return Jwts.parserBuilder()
-            .setSigningKey(key)
-            .build()
-            .parseClaimsJws(token)
-            .getHeader();
+                .setSigningKey(key)
+                .build()
+                .parseClaimsJws(token)
+                .getHeader();
     }
+
+//    public boolean isValidateToken(String token) {
+//        Jwts.parserBuilder().setSigningKey(key)
+//                .build().parseClaimsJws(token).getBody();
+//        return true;
+//    }
 
     public boolean isValidateToken(String token) throws JwtException {
         try {
             Jwts.parserBuilder().setSigningKey(key)
-                .build().parseClaimsJws(token).getBody();
+                    .build().parseClaimsJws(token).getBody();
             return true;
         } catch (SecurityException | MalformedJwtException | UnsupportedJwtException |
                  IllegalArgumentException e) {
             throw new SignatureException(INVALID_TOKEN.getMessage());
         } catch (ExpiredJwtException e) {
             throw new ExpiredJwtException(parseHeader(token), parseClaims(token),
-                EXPIRED_TOKEN.getMessage());
+                    EXPIRED_TOKEN.getMessage());
         }
     }
 }
