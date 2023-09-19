2624806b10507d6235040993955d5c7abefb008f
김민태 <kimmintae981019@gmail.com>
2023-09-19 17:22:19 +0900

    Feat: 스크랩 검색 UI 구현

--- frontend/src/components/user/mypage/left/MyPageLeftTop.jsx
+++ frontend/src/components/user/mypage/left/MyPageLeftTop.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import styled from 'styled-components';
-import MyPageTabs from 'components/user/mypage/left/tabs/MyPageTabs';
-
+import MyPageTabs from 'components/user/mypage/left/top/MyPageTabs';
+import ScrapSearch from 'components/user/mypage/left/top/ScrapSearch'
 
 export default function MyPageLeftTop({userId, tabs}) {
     return (
         <LeftTopContainer>
             <MyPageTabs tabs={tabs}></MyPageTabs>
+            <ScrapSearch></ScrapSearch>
         </LeftTopContainer>
     );
 }
 
 
 const LeftTopContainer = styled.div`
+    display: flex;
+    position: relative;
 `;
