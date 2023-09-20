bbab8a127bf7bd1fc7703c9f2462cc36f57a92b6
Seulgi <urina842@gmail.com>
2023-09-20 17:35:02 +0900

    Refactor: 빈칸 없애기

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -7,8 +7,8 @@ export default function Login() {
     const KAKAO_CLIENT_ID = process.env.REACT_APP_KAKAO_CLIENT_ID;
     const KAKAO_REDIRECT_URI = process.env.REACT_APP_KAKAO_REDIRECT_URI;
 
-    const googleLink = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${GOOGLE_CLIENT_ID}&redirect_uri=${GOOGLE_REDIRECT_URI}&response_type=code&scope=email`
-    const kakaoLink = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_CLIENT_ID }&redirect_uri=${KAKAO_REDIRECT_URI}&response_type=code`;
+    const googleLink = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${GOOGLE_CLIENT_ID}&redirect_uri=${GOOGLE_REDIRECT_URI}&response_type=code&scope=email`;
+    const kakaoLink = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_CLIENT_ID}&redirect_uri=${KAKAO_REDIRECT_URI}&response_type=code`;
 
     const googleLogin = () => {
         window.location.href = googleLink;
