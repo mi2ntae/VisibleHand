6f21c957c411091a1f1529c33230078011f0e559
Seulgi <urina842@gmail.com>
2023-09-20 10:49:40 +0900

    Refactor: 메서드명 변경

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function KakaoLogin() {
+export default function Login() {
     const GOOGLE_CLIENT_ID=process.env.REACT_APP_GOOGLE_CLIENT_ID;
     const GOOGLE_REDIRECT_URI=process.env.REACT_APP_GOOGLE_REDIRECT_URI;
 
@@ -10,12 +10,14 @@ export default function KakaoLogin() {
     const googleLink = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${GOOGLE_CLIENT_ID}&redirect_uri=${GOOGLE_REDIRECT_URI}&response_type=code&scope=email`
     const kakaoLink = `https://kauth.kakao.com/oauth/authorize?client_id=${KAKAO_CLIENT_ID }&redirect_uri=${KAKAO_REDIRECT_URI}&response_type=code`;
 
-    const kakaoLogin = () => {
-        window.location.href = kakaoLink;
-    };
     const googleLogin = () => {
         window.location.href = googleLink;
+
     };
+    const kakaoLogin = () => {
+        window.location.href = kakaoLink;
+    };  
+
 
     return (
         <div>
