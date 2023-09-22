971cff1f4d5ca93546abbde2b3659fc46e112d55
Seulgi <urina842@gmail.com>
2023-09-22 17:53:18 +0900

    Refactor: 로그인 버튼

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import "./Login.css";
+import LoginBtn from "components/user/login/LoginBtn";
 
 export default function Login() {
     const GOOGLE_CLIENT_ID=process.env.REACT_APP_GOOGLE_CLIENT_ID;
@@ -18,16 +20,19 @@ export default function Login() {
         window.location.href = kakaoLink;
     };  
 
-
     return (
+        //중앙으로 옮기는 컴포넌트로 감싸기
         <div>
-            <button type='button' onClick={googleLogin}>
-                구글 로그인
-            </button>
-            <button type='button' onClick={kakaoLogin}>
-                카카오 로그인
-            </button>
+            <LoginBtn background={"white"} title={"구글로 시작하기"} onClick={googleLogin}
+                imgWidth={25} imgHeight={25} imgSrc={"https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Google_%22G%22_Logo.svg/2048px-Google_%22G%22_Logo.svg.png"} imgAlt={"Google Logo"}
+                imgPadding={10}
+            ></LoginBtn>
+            <LoginBtn background={"#F7E600"} title={"카카오로 시작하기"} onClick={kakaoLogin}
+                imgWidth={45} imgHeight={45} imgSrc={"https://cs.kakao.com/img/cskakaocom/pc/thumb/thumb_kakaotalk.png"} imgAlt={"Kakao Logo"}
+            ></LoginBtn>
         </div>
+        
         
     );
 }
+
