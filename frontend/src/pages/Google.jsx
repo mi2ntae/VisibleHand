048355c4ded93a282f3f312bf45fd945f4cae560
Seulgi <urina842@gmail.com>
2023-09-22 17:53:36 +0900

    Refactor: 로그인 연결

--- frontend/src/pages/Google.jsx
+++ frontend/src/pages/Google.jsx
@@ -1,27 +1,32 @@
 import React, {useEffect} from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { setUser } from "reducer/userReducer";
+import http from "api/commonHttp";
 
 export default function Google() {
-    useEffect(() => {
-        const code = new URL(window.location.href).searchParams.get("code");
-        console.log(code);
+  const user = useSelector((state) => state.user);
+  console.log(user);
 
-        if (code) {
-            fetch(process.env.REACT_APP_HTTP_URL + '/api/user/auth/google?code=' + code, {
-                method: 'GET'
-            })
-            .then((response) => response.json())
-            .then((data) => {
-              const accessToken = data.token.accessToken;
-              const refreshToken = data.token.refreshToken;
-              console.log(accessToken);
-              console.log(refreshToken);
-              // dispatch({ type: 'setUser', payload: { accessToken: accessToken, refreshToken: refreshToken } });
-          });
-        }
-      }, []);
-    
-      return (
-        <div>
-        </div>
-      );
+  const dispatch = useDispatch();   
+
+  useEffect(() => {
+      const code = new URL(window.location.href).searchParams.get("code");
+
+      if(code) {
+         http
+        .get("user/auth/google?code=" + code)
+        .then(({ data }) => {
+          dispatch(setUser(data));
+        })
+        .catch((err) => {
+          console.log(err);
+        }); 
+      }
+      
+    }, []);
+
+  return (
+      <div>
+      </div>
+  );
 }
--- frontend/src/pages/Kakao.jsx
+++ frontend/src/pages/Kakao.jsx
@@ -1,27 +1,39 @@
 import React, {useEffect} from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { setUser } from "reducer/userReducer";
+import http from "api/commonHttp";
 
 export default function Kakao() {
+    const user = useSelector((state) => state.user);
+    console.log(user);
+
+    const dispatch = useDispatch();   
+
     useEffect(() => {
         const code = new URL(window.location.href).searchParams.get("code");
-        console.log(code);
 
-        if (code) {
-            fetch(process.env.REACT_APP_HTTP_URL + '/api/user/auth/kakao?code=' + code, {
-                method: 'GET'
-            })
-            .then((response) => response.json())
-            .then((data) => {
-                const accessToken = data.token.accessToken;
-                const refreshToken = data.token.refreshToken;
-                console.log(accessToken);
-                console.log(refreshToken);
-                // dispatch({ type: 'setUser', payload: { accessToken: accessToken, refreshToken: refreshToken } });
-            });
+        if(code) {
+           http
+          .get("user/auth/kakao?code=" + code)
+          .then(({ data }) => {
+            dispatch(setUser(data));
+          })
+          .catch((err) => {
+            console.log(err);
+          }); 
         }
+        
       }, []);
+    
 
-    return (
-        <div>
-        </div>
-    );
+  return (
+    // 컴포넌트로 바꾸기
+    <div>
+        <strong>프로필 설정</strong><br></br>
+        닉네임 <input type="text" name="nickname" placeholder='닉네임을 입력하세요.' />
+        상태메시지 <input type="text" name="stateMsg" placeholder='상태메시지를 입력하세요.' />
+        이메일 <input type="text" name="snsEmail" value={user.snsEmail} readOnly />
+    </div>
+  );
 }
+
