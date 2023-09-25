63beba55bb7739e11ee1d7fa30ca56d0e4d6944f
김민태 <kimmintae981019@gmail.com>
2023-09-25 10:40:57 +0900

    Feat: 마이페이지 오답노트 검색창 제거 및 페이지 구현

--- frontend/src/components/user/mypage/left/MyPageLeftTop.jsx
+++ frontend/src/components/user/mypage/left/MyPageLeftTop.jsx
@@ -2,12 +2,20 @@ import React from 'react';
 import styled from 'styled-components';
 import MyPageTabs from 'components/user/mypage/left/top/MyPageTabs';
 import MypageSearch from 'components/user/mypage/left/top/MypageSearch'
+import { useSelector } from 'react-redux';
 
 export default function MyPageLeftTop({userId, tabs}) {
+    const tabNo = useSelector((state) => state.mypageTab.tabNo)
     return (
         <LeftTopContainer>
             <MyPageTabs tabs={tabs}></MyPageTabs>
+            {
+            tabNo != 2
+            ?
             <MypageSearch></MypageSearch>
+            :
+            <span></span>
+            }
         </LeftTopContainer>
     );
 }
