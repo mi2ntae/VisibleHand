f5f49fe00372e37ec8a4bcd94978ba8d4c3e626a
Seulgi <urina842@gmail.com>
2023-09-25 15:22:16 +0900

    Refactor: 로그인 로딩 페이지

---
+++ frontend/src/components/user/login/Loading.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BeatLoader } from 'react-spinners';
+import styled from "styled-components";
+
+function Loading() {
+    return (
+        <ContentWrap>
+            <Container>
+                <BeatLoader color="#6A74C9" />
+            </Container>
+        </ContentWrap>
+    );
+  }
+  
+  export default Loading;
+
+const ContentWrap = styled.div`
+    position: relative;
+    height: 100vh;
+`;
+const Container = styled.div`
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+`;
+  
