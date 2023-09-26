3f280e3bcea61e57f2be767940904525df981254
Seulgi <urina842@gmail.com>
2023-09-26 15:36:00 +0900

    Refactor: 프로필 등록 페이지

--- frontend/src/components/user/login/Profile.jsx
+++ frontend/src/components/user/login/Profile.jsx
@@ -1,17 +1,69 @@
-import React from 'react';
-import { useSelector} from 'react-redux';
+import React, { useState } from 'react';
+import style from './Profile.css';
 
-function Profile({onSubmit, onChange, onClick, value}) {
-   return (
-    <form onSubmit={onSubmit}>
-        <strong>프로필 설정</strong><br />
-        프로필 사진 <input type="file" name="file"></input><br />
-        닉네임 <input type="text" name="nickname" placeholder='닉네임을 입력하세요.' onChange={onChange}/>
-        <button type="button" onClick={onClick}>중복확인</button><br />
-        상태메시지 <input type="text" name="stateMsg" placeholder='상태메시지를 입력하세요.' /><br />
-        이메일 <input type="text" name="snsEmail" value={value} readOnly /><br />
-        <button type="submit">등록</button>
-    </form>
+function Profile({onSubmit, img, imgClick, imgRef, imgChange, nickChange, nickClick, msgChange, value, nickError, msgError }) {
+  const [dropdown, setDropdown] = useState(false);
+
+  const dropdownOpen = () => {
+    console.log("click");
+    setDropdown(!dropdown);
+  };
+
+  const handleMouseLeave = () => {
+    setDropdown(false);
+  };
+
+    return (
+    <div class="form-container">
+        <form class="form" onSubmit={onSubmit}>
+            <div class="center" >
+                 <div class="title">프로필 설정</div>
+            </div>
+           <br />
+            <div class="center">
+               <div class="profile-container" onMouseLeave={handleMouseLeave}>
+                    <img class="profile" src={img} onClick={() => dropdownOpen()}/>
+                    {dropdown &&
+                    <div class="dropdown">
+                        <button type="button" onClick={imgClick}>이미지 등록</button><br />
+                        <button type="button">이미지 삭제</button>
+                    </div>}
+                    {/* <label class="profileImg-label" htmlFor="profileImg">프로필 이미지 추가</label> */}
+                    <input id="profileImg" class="profileImg" type="file" name="file" ref={imgRef} onChange={imgChange}></input><br />
+                </div> 
+            </div>
+            <div>
+                닉네임 {nickError && <p class="invalid">{nickError}</p>}
+            </div>
+            
+            <div class="input-container">
+               <input class="inputNick" type="text" name="nickname" 
+                placeholder='닉네임을 입력하세요.' onChange={nickChange}/>
+                <button class="dupbtn" type="button" onClick={nickClick}>중복확인</button>
+            </div>
+        
+            <div>
+                상태메시지 {msgError && <p class="invalid">{msgError}</p>}
+            </div>
+            
+            <div>
+                <textarea name="stateMsg"  
+                placeholder='상태메시지를 입력하세요.' onChange={msgChange}/>
+            </div>
+            
+            
+            <div>
+                이메일
+            </div>
+            <div>
+                <input class="inputEmail" type="text" name="snsEmail" value={value} readOnly />
+            </div>
+            <div>
+                <button class="subbtn" type="submit">등록</button>
+            </div>
+        </form>
+    </div>
+   
    )
 }
   
--- frontend/src/pages/ProfileSetting.jsx
+++ frontend/src/pages/ProfileSetting.jsx
@@ -1,4 +1,4 @@
-import React,  {useState} from 'react';
+import React,  {useRef, useState} from 'react';
 import { useSelector} from 'react-redux';
 import { setUser } from "reducer/userReducer";
 import http from "api/commonHttp";
@@ -10,17 +10,107 @@ export default function ProfileSetting() {
 
   const navigate = useNavigate();
 
-  const [nickname, setNickname] = useState("");
+  //프로필 사진
+  const imgUrl = "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png";
+  const [image, setImage] = useState(imgUrl);
+  const [file, setFile] = useState("");
+  const inputFile = useRef();
 
-  const onChange = (event) => {
+  const onFile = (event) => {
+    if(event.target.files[0]) {
+      setFile(event.target.files[0]);
+    } else {
+      setImage(imgUrl);
+      return;
+    }
+
+    const reader = new FileReader();
+    reader.onload = () => {
+      if(reader.readyState===2) {
+        setImage(reader.result);
+      }
+    }
+    reader.readAsDataURL(event.target.files[0]);
+  }
+
+  //드롭다운
+  const [dropdown, setDropdown] = useState(false);
+
+  const dropdownOpen = () => {
+    setDropdown(!dropdown);
+  };
+
+  //닉네임, 상태메시지 유효성
+  const [nickname, setNickname] = useState("");
+  const [stateMsg, setStateMsg] = useState("");
+
+  const onChangeNick = (event) => {
     const newNickname = event.target.value;
     setUser({ ...user, nickname: newNickname });
     setNickname(newNickname);
+    
+    if(newNickname.length>32) {
+      setErrors((prev) => ({
+        ...prev,
+        nickname: {
+          ...prev.nickname,
+          invalid: true,
+        },
+      }));
+    } else {
+      setErrors((prev) => ({
+        ...prev,
+        nickname: {
+          ...prev.nickname,
+          invalid: false,
+        },
+      }));
+    }
   }
 
+  const onChangeMsg = (event) => {
+    const newStateMsg = event.target.value;
+    setStateMsg(newStateMsg);
+    
+    if(newStateMsg.length>128) {
+      setErrors((prev) => ({
+        ...prev,
+        stateMsg: {
+          ...prev.stateMsg,
+          invalid: true,
+        },
+      }));
+    } else {
+      setErrors((prev) => ({
+        ...prev,
+        stateMsg: {
+          ...prev.stateMsg,
+          invalid: false,
+        },
+      }));
+    }
+  }
+
+  const [errors, setErrors] = useState({
+    nickname: {
+        invalid: false,
+        message: "닉네임은 32글자 이하여야 합니다.",
+    },
+    stateMsg: {
+        invalid: false,
+        message: "상태메시지는 128자 이하여야 합니다.",
+    }
+  })
+ 
+  //닉네임 중복 체크
   const [dupCheck, setDupCheck] = useState(false);
 
   const isDuplicatedNick = (nickname) => {
+    if(nickname==null || nickname==="") {
+      alert("닉네임을 입력해주세요.");
+      return;
+    }
+
     http.get('/user/auth?nickname='+nickname)
     .then(({ data }) => {
       if(data.isDuplicated===0) {
@@ -36,13 +126,15 @@ export default function ProfileSetting() {
     });  
   }
 
+  //프로필 등록
   const useSubmit = (event) => {
+    event.preventDefault();
+
     if(!dupCheck) {
       alert("닉네임 중복 확인을 완료해주세요.");
       return;
     }
 
-    event.preventDefault()
     const fileInput = event.target.querySelector('input[type="file"]');
     const formData = new FormData();
 
@@ -83,8 +175,17 @@ export default function ProfileSetting() {
 
   return (
     <Profile onSubmit={useSubmit}
-    onChange={onChange}
-    onClick={() => isDuplicatedNick(nickname)}
-    value={user.snsEmail}></Profile>
+      img={image}
+      imgClick={() =>  inputFile.current.click()}
+      imgRef={inputFile}
+      imgChange={onFile}
+      nickChange={onChangeNick}
+      nickClick={() => isDuplicatedNick(nickname)}
+      msgChange={onChangeMsg}
+      value={user.snsEmail}
+      nickError={errors.nickname.invalid ? errors.nickname.message : ''}
+      msgError={errors.stateMsg.invalid ? errors.stateMsg.message : ''}
+    >
+    </Profile>
   );
 }
