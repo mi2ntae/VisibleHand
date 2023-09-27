0200d6e55ebfe9c98fb1ae80ad4a2421a4431f64
Seulgi <urina842@gmail.com>
2023-09-27 15:07:42 +0900

    Refactor: 프로필 컴포넌트 수정

--- frontend/src/components/user/login/Profile.jsx
+++ frontend/src/components/user/login/Profile.jsx
@@ -1,65 +1,91 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
+import { useSelector } from 'react-redux';
 import style from './Profile.css';
 
-function Profile({onSubmit, img, imgClick, imgRef, imgChange, nickChange, nickClick, msgChange, value, nickError, msgError }) {
-  const [dropdown, setDropdown] = useState(false);
+function Profile({title, onSubmit, nickChange, nickClick, msgChange, nickError, msgError, nickValue, imgValue, msgValue }) {
+  const user = useSelector((state) => state.user);
 
-  const dropdownOpen = () => {
-    console.log("click");
-    setDropdown(!dropdown);
-  };
+  //프로필 사진
+  const imgUrl = "https://visiblehand-bucket.s3.ap-northeast-2.amazonaws.com/user_default.png";
+  const [image, setImage] = useState(imgUrl);
+  const [file, setFile] = useState("");
+  const inputFile = useRef();
 
-  const handleMouseLeave = () => {
-    setDropdown(false);
-  };
+  const onFile = (event) => {
+    if(event.target.files[0]) {
+      setFile(event.target.files[0]);
+    } else {
+      setImage(imgUrl);
+      return;
+    }
+
+    const reader = new FileReader();
+    reader.onload = () => {
+      if(reader.readyState===2) {
+        setImage(reader.result);
+      }
+    }
+        reader.readAsDataURL(event.target.files[0]);
+    }
+
+    const [dropdown, setDropdown] = useState(false);
+
+    const handleMouseLeave = () => {
+        setDropdown(false);
+    };
 
     return (
     <div class="form-container">
         <form class="form" onSubmit={onSubmit}>
-            <div class="center" >
-                 <div class="title">프로필 설정</div>
-            </div>
-           <br />
             <div class="center">
-               <div class="profile-container" onMouseLeave={handleMouseLeave}>
-                    <img class="profile" src={img} onClick={() => dropdownOpen()}/>
-                    {dropdown &&
-                    <div class="dropdown">
-                        <button type="button" onClick={imgClick}>이미지 등록</button><br />
-                        <button type="button">이미지 삭제</button>
-                    </div>}
-                    {/* <label class="profileImg-label" htmlFor="profileImg">프로필 이미지 추가</label> */}
-                    <input id="profileImg" class="profileImg" type="file" name="file" ref={imgRef} onChange={imgChange}></input><br />
-                </div> 
+                <div class="title">{title}</div>
             </div>
-            <div>
-                닉네임 {nickError && <p class="invalid">{nickError}</p>}
+            <div class="center">
+               <div class="profile-container" onMouseEnter={() => setDropdown(true)} onMouseLeave={handleMouseLeave}>
+                        <img class="profile" src={image} />
+                        {dropdown && 
+                        <div class="dropdown">
+                            <button class="imgbtn" type="button" onClick={() =>  inputFile.current.click()}>이미지 등록</button><br />
+                            <button class="imgbtn" type="button" onClick={() => setImage(imgUrl)}>이미지 삭제</button>
+                        </div>
+                        }
+                        <input id="profileImg" class="profileImg" type="file" name="file" value={imgValue} ref={inputFile} onChange={onFile}></input><br />
+                    </div>
             </div>
-            
-            <div class="input-container">
-               <input class="inputNick" type="text" name="nickname" 
-                placeholder='닉네임을 입력하세요.' onChange={nickChange}/>
-                <button class="dupbtn" type="button" onClick={nickClick}>중복확인</button>
-            </div>
-        
-            <div>
-                상태메시지 {msgError && <p class="invalid">{msgError}</p>}
-            </div>
-            
-            <div>
-                <textarea name="stateMsg"  
-                placeholder='상태메시지를 입력하세요.' onChange={msgChange}/>
-            </div>
+            <div class="center">   
+                <div class="name">
+                    닉네임 {nickError && <p class="invalid">{nickError}</p>}
+                </div>
             
             
-            <div>
-                이메일
-            </div>
-            <div>
-                <input class="inputEmail" type="text" name="snsEmail" value={value} readOnly />
-            </div>
-            <div>
-                <button class="subbtn" type="submit">등록</button>
+                <div class="input-container">
+                <input class="inputNick" type="text" name="nickname" value={nickValue}
+                    placeholder='닉네임을 입력하세요.' onChange={nickChange}/>
+                    <button class="dupbtn" type="button" onClick={nickClick}>중복확인</button>
+                </div>
+           
+                <div class="name">
+                    상태메시지 {msgError && <p class="invalid">{msgError}</p>}
+                </div>
+                <div>
+                    <div class="input-container">
+                        <textarea name="statusMsg" value={msgValue}
+                        placeholder='상태메시지를 입력하세요.' onChange={msgChange}/>
+                    </div>
+                </div>
+                
+                <div class="name">
+                    이메일
+                </div>
+                <div>
+                    <div class="input-container" >
+                        <input class="inputEmail" type="text" name="snsEmail" value={user.snsEmail} readOnly />
+                    </div>
+                    
+                </div>
+                <div class="centerbtn">
+                    <button class="subbtn" type="submit">등록</button>
+                </div>
             </div>
         </form>
     </div>
