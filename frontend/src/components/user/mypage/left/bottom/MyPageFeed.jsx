656b25a54aaf8f34a088f56307764610dc76e4ea
김민태 <kimmintae981019@gmail.com>
2023-09-27 14:52:26 +0900

    Feat: 마이페이지 피드 조회

--- frontend/src/components/user/mypage/left/bottom/MyPageFeed.jsx
+++ frontend/src/components/user/mypage/left/bottom/MyPageFeed.jsx
@@ -1,20 +1,88 @@
-import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import React, { useEffect, useState, useRef } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import styled from 'styled-components';
 import { setKeyword } from 'reducer/mypageTabReducer';
+import http from 'api/commonHttp';
+import UserElement from "components/Feed/UserElement";
+import FeedElement from "components/Feed/FeedElement";
+import { background } from "lib/style/colorPalette";
 
-export default function Mypage() {
+export default function MypageFeed({userId}) {
     const dispatch = useDispatch();
+    const searchType = useSelector((state) => state.mypageTab.searchType);
+    const keyword = useSelector((state) => state.mypageTab.keyword);
+    const [feeds, setFeeds] = useState([]);
+    const page = useRef(1);
+    const [showObserver, setShowObserver] = useState(true);
+    const target = useRef(null);
+    const [loading, setLoading] = useState(false);
 
-    // useEffect(() => {
-    //     return () => {
-    //         dispatch(setKeyword(""));
-    //     }
-    // }, [])
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (!entry.isIntersecting) return;
+            getData();
+        });
+    });
+
+    const getData = () => {
+        let uri = ``;
+        if(keyword) uri = `user/feed/${userId}?searchType=${searchType}&keyword=${keyword}&page=${page.current-1}`
+        else uri = `user/feed/${userId}?searchType=${searchType}&page=${page.current-1}`
+        http
+        .get(uri)
+        .then((data) => {
+            setFeeds((prevFeeds) => [...prevFeeds, ...data.data])
+            if(data.data.length > 0) page.current += 1;
+        })
+        .catch((err) => {
+            alert(err);
+        });
+    }
+
+    const getInitData = () => {
+        page.current = 1;
+        let uri = ``;
+        if(keyword) uri = `user/feed/${userId}?searchType=${searchType}&keyword=${keyword}&page=${page.current-1}`
+        else uri = `user/feed/${userId}?searchType=${searchType}&page=${page.current-1}`
+        http
+        .get(uri)
+        .then((data) => {
+            setFeeds(data.data)
+            if(data.data.length > 0) page.current += 1;
+        })
+        .catch((err) => {
+            alert(err);
+        });
+    }
+
+    
+    useEffect(() => {
+        observer.observe(target.current);
+        setLoading(true);
+    }, []);
+    
+    useEffect(() => {
+        if(!loading) return;
+        setTimeout(() => getInitData(), 10)
+    }, [keyword])
 
     return (
         <div>
             <FeedContainer>
+            {
+            feeds.map((data) => {
+                return <FeedElement data={data} key={data.feedId} />;
+            })
+            }
+            {showObserver ? (
+                <div
+                id="scrollEnd"
+                style={{ height: "1px", backgroundColor: background }}
+                ref={target}
+                ></div>
+            ) : (
+                ""
+            )}
             </FeedContainer>
         </div>
     );
@@ -22,10 +90,13 @@ export default function Mypage() {
 
 const FeedContainer = styled.div`
     display: flex;
-    justify-content: flex-start;
     margin: auto;
     padding: 20px;
     box-sizing: border-box;
-    background-color: red;
     width: 100%;
+    max-height: 500px;
+    gap: 24px;
+    background-color: ;
+    flex-direction: column;
+    overflow: auto;
 `;
--- frontend/src/pages/Mypage.jsx
+++ frontend/src/pages/Mypage.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import styled from 'styled-components';
 import { useDispatch } from 'react-redux';
-import { setTabNo } from '../reducer/mypageTabReducer';
+import { setKeyword, setSearchType, setTabNo } from '../reducer/mypageTabReducer';
 import { useParams } from 'react-router';
 import MyPageLeft from 'components/user/mypage/MyPageLeft';
 import MyPageRight from 'components/user/mypage/MyPageRight';
@@ -13,6 +13,8 @@ export default function Mypage() {
     useEffect(() => {
         return() => {
             dispatch(setTabNo(0));
+            dispatch(setKeyword(""));
+            dispatch(setSearchType(0));
         }
     // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
