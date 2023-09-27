c10be1906706a11f14a21957bed08f3c1ba89dbf
Seulgi <urina842@gmail.com>
2023-09-27 13:11:11 +0900

    Refactor: 다시 get으로 변경

--- backend/vh/src/main/java/com/it/vh/user/api/AuthController.java
+++ backend/vh/src/main/java/com/it/vh/user/api/AuthController.java
@@ -5,6 +5,7 @@ import com.it.vh.user.service.AuthUserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,7 +20,7 @@ public class AuthController {
 
     private final AuthUserService oauthService;
 
-    @PostMapping("/auth/{provider}")
+    @GetMapping("/auth/{provider}")
     public ResponseEntity<LoginResDto> login(@PathVariable(name = "provider") String provider,
         @RequestParam(name = "code") String code) {
         LoginResDto loginResDto = oauthService.login(code, provider);
