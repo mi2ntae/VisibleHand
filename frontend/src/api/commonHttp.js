e2c182200bf50c5931b16a80e32fcb8706ee718f
seul <l.23.53.l2l9@gmail.com>
2023-09-30 01:00:10 +0900

    Feat: refreshToken 확인 후 액세스 토큰 재발급 추가

--- frontend/src/api/commonHttp.js
+++ frontend/src/api/commonHttp.js
@@ -1,29 +1,72 @@
 import axios from "axios";
 import { useEffect } from "react";
-import store from "../store.js"; //
-
+import store from "../store.js";
+import { useDispatch } from 'react-redux';
+import { setUser } from "reducer/userReducer";
 const http = axios.create({
   baseURL: process.env.REACT_APP_HTTP_URL + "/api/",
 });
 
 const Interceptor = ({ children }) => {
+  const dispatch = useDispatch();
+
   useEffect(() => {
     http.interceptors.request.use(
       function (config) {
-        config.headers.Authorization = `Bearer ${store.getState().user.accessToken}`;
-        return config;
+        if (!config.url.includes("/token/")) {
+          config.headers.Authorization = `Bearer ${store.getState().user.accessToken}`;
+          return config;
+        } else {
+          delete config.headers.Authorization;
+          return config;
+        }
       },
-      function (err) {
-        return Promise.reject(err);
+      function (error) {
+        return Promise.reject(error);
       }
     );
     http.interceptors.response.use(
       (response) => {
         return response;
       },
-      (err) => {
-        alert(err);
-        return Promise.reject(err);
+      async (error) => {
+        if (error.response.data.message === "만료된 토큰") {
+
+        const user = store.getState().user;
+        const refreshToken = user.refreshToken;
+        const userId = user.userId;
+
+        await http
+        .get( "user/auth/token/" + userId,
+        { headers: { 'refreshToken': `${store.getState().user.refreshToken}` },
+        })
+        .then(response => {
+          if(response.data.message==="토큰 재발급 완료") {
+            const newAccessToken = response.data.tokenInfo.accessToken;
+            const newRefreshToken = response.data.tokenInfo.refreshToken;
+            console.log(newAccessToken);
+            console.log(newRefreshToken);
+
+            dispatch(setUser({
+                  token: {
+                    accessToken: newAccessToken,
+                    refreshToken: newRefreshToken,
+                  },
+                  user: {
+                    userId: user.userId,
+                    nickname: user.nickname,
+                    snsEmail: user.snsEmail,
+                    provider: user.provider
+                  }
+              }));
+              
+              return error.config;
+          }
+        })
+        } else {
+          alert(error);
+        return Promise.reject(error);
+        }
       }
     );
   }, []);
