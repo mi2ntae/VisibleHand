4080c245e8bd7c6582502caea2bfd50e080114ef
seul <l.23.53.l2l9@gmail.com>
2023-09-30 21:39:56 +0900

    Refactor: 기본값 전달되도록 변경

--- frontend/src/components/user/login/Profile.jsx
+++ frontend/src/components/user/login/Profile.jsx
@@ -1,32 +1,11 @@
-import React, { useRef, useState } from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import style from './Profile.css';
 
-function Profile({title, onSubmit, nickChange, nickClick, msgChange, nickError, msgError, nickValue, imgValue, msgValue }) {
+function Profile({title, onSubmit, inputImg, deleteImg, imgRef, imgChange, nickChange, nickClick, msgChange, nickError, msgError, nickValue, imgValue, msgValue }) {
   const user = useSelector((state) => state.user);
 
-  //프로필 사진
-  const imgUrl = "https://visiblehand-bucket.s3.ap-northeast-2.amazonaws.com/user_default.png";
-  const [image, setImage] = useState(imgUrl);
-  const [file, setFile] = useState("");
-  const inputFile = useRef();
-
-  const onFile = (event) => {
-    if(event.target.files[0]) {
-      setFile(event.target.files[0]);
-    } else {
-      setImage(imgUrl);
-      return;
-    }
-
-    const reader = new FileReader();
-    reader.onload = () => {
-      if(reader.readyState===2) {
-        setImage(reader.result);
-      }
-    }
-        reader.readAsDataURL(event.target.files[0]);
-    }
+    const imgUrl = "https://visiblehand-bucket.s3.ap-northeast-2.amazonaws.com/user_default.png";
 
     const [dropdown, setDropdown] = useState(false);
 
@@ -42,14 +21,14 @@ function Profile({title, onSubmit, nickChange, nickClick, msgChange, nickError, 
             </div>
             <div class="center">
                <div class="profile-container" onMouseEnter={() => setDropdown(true)} onMouseLeave={handleMouseLeave}>
-                        <img class="profile" src={image} />
+                        <img class="profile" src={imgValue!=null && imgValue!="" ? imgValue : imgUrl} />
                         {dropdown && 
                         <div class="dropdown">
-                            <button class="imgbtn" type="button" onClick={() =>  inputFile.current.click()}>이미지 등록</button><br />
-                            <button class="imgbtn" type="button" onClick={() => setImage(imgUrl)}>이미지 삭제</button>
+                            <button class="imgbtn" type="button" onClick={inputImg}>이미지 등록</button><br />
+                            <button class="imgbtn" type="button" onClick={deleteImg}>이미지 삭제</button>
                         </div>
                         }
-                        <input id="profileImg" class="profileImg" type="file" name="file" value={imgValue} ref={inputFile} onChange={onFile}></input><br />
+                        <input id="profileImg" class="profileImg" type="file" name="file" ref={imgRef} onChange={imgChange}></input><br />
                     </div>
             </div>
             <div class="center">   
@@ -59,7 +38,7 @@ function Profile({title, onSubmit, nickChange, nickClick, msgChange, nickError, 
             
             
                 <div class="input-container">
-                <input class="inputNick" type="text" name="nickname" value={nickValue}
+                <input class="inputNick" type="text" name="nickname" defaultValue={nickValue!="" ? nickValue : ""} 
                     placeholder='닉네임을 입력하세요.' onChange={nickChange}/>
                     <button class="dupbtn" type="button" onClick={nickClick}>중복확인</button>
                 </div>
@@ -69,7 +48,7 @@ function Profile({title, onSubmit, nickChange, nickClick, msgChange, nickError, 
                 </div>
                 <div>
                     <div class="input-container">
-                        <textarea name="statusMsg" value={msgValue}
+                        <textarea name="statusMsg" defaultValue={msgValue!="" ? msgValue : ""}
                         placeholder='상태메시지를 입력하세요.' onChange={msgChange}/>
                     </div>
                 </div>
