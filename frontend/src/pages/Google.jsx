e355ea93a7b78af657ebac8f8b02e4d6068bcf68
seul <l.23.53.l2l9@gmail.com>
2023-10-02 23:03:07 +0900

    Refactor: 이미 가입한 회원이면 바로 뉴스 페이지로 이동하도록 변경

--- frontend/src/pages/Google.jsx
+++ frontend/src/pages/Google.jsx
@@ -20,7 +20,7 @@ export default function Google() {
         .then(({ data }) => {
           dispatch(setUser(data));
           if(data.user.isAlready===1) {
-            navigate('/');
+            navigate('/news');
           } else if(data.user.isAlready===0) {
             navigate('/profile');
           }
