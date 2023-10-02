11b28f90a12635176952e5b76d5b2cee14bb5ed5
seul <l.23.53.l2l9@gmail.com>
2023-10-02 23:00:26 +0900

    Docs: swagger 추가

--- backend/vh/src/main/java/com/it/vh/user/api/AuthController.java
+++ backend/vh/src/main/java/com/it/vh/user/api/AuthController.java
@@ -3,6 +3,7 @@ package com.it.vh.user.api;
 import com.it.vh.common.util.jwt.dto.TokenInfo;
 import com.it.vh.user.api.dto.auth.LoginResDto;
 import com.it.vh.user.service.AuthUserService;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +17,7 @@ public class AuthController {
 
     private final AuthUserService oauthService;
 
+    @ApiOperation(value = "유저 소셜 로그인", notes = "유저가 가입 또는 로그인합니다.")
     @GetMapping("/auth/{provider}")
     public ResponseEntity<LoginResDto> login(@PathVariable(name = "provider") String provider,
                                              @RequestParam(name = "code") String code) {
@@ -23,6 +25,7 @@ public class AuthController {
         return ResponseEntity.ok().body(loginResDto);
     }
 
+    @ApiOperation(value = "유저 토큰 재발급", notes = "유저의 토큰이 만료된 경우 확인 후 재빌급합니다.")
     @GetMapping("/auth/token/{userId}")
     public ResponseEntity<TokenInfo> setToken(@PathVariable Long userId) {
         TokenInfo token = oauthService.setToken(userId);
