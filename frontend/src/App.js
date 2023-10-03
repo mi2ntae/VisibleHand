5cc96584a25e616bacace6a208957c4ab719e110
seul <l.23.53.l2l9@gmail.com>
2023-10-03 18:10:42 +0900

    Refactor: 다시 원래대로 변경

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,27 +5,27 @@ import { useSelector } from "react-redux";
 import OnBoarding from "pages/Onboarding";
 
 function App() {
-  const path = window.location.pathname;
+  // const path = window.location.pathname;
 
-  const notSidebar = path === '/login' || path === '/auth/kakao' || path === '/auth/google' || path === '/profile';
-  const user = useSelector((state) => state.user);
+  // const notSidebar = path === '/login' || path === '/auth/kakao' || path === '/auth/google' || path === '/profile';
+  // const user = useSelector((state) => state.user);
 
   return (
     <Root>
       <GlobalStyle />
-      {(user.nickname==="" || user.nickname===null || notSidebar)
+      {/* {(user.nickname==="" || user.nickname===null || notSidebar)
       ?
       <Content>
         <Outlet />
         </Content>
       :
-      <>
+      <> */}
       <SideBar />
           <Content>
             <Outlet />
           </Content>
-      </>
-      }
+      {/* </> */}
+      {/* } */}
     </Root>
   );
 }
