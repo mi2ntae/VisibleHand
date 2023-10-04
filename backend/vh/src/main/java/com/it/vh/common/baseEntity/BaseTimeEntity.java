94fdfeab105f02956279be93969df5302a3df1b9
김민태 <kimmintae981019@gmail.com>
2023-10-04 10:26:12 +0900

    Fix: BaseTimeEntity createdAt에 마지막 수정일 적용

--- backend/vh/src/main/java/com/it/vh/common/baseEntity/BaseTimeEntity.java
+++ backend/vh/src/main/java/com/it/vh/common/baseEntity/BaseTimeEntity.java
@@ -2,6 +2,7 @@ package com.it.vh.common.baseEntity;
 
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.Column;
@@ -15,5 +16,6 @@ import java.time.LocalDateTime;
 public abstract class BaseTimeEntity {
 	@Column(nullable = false, columnDefinition = "DATETIME")
 	@CreatedDate
+	@LastModifiedDate
 	private LocalDateTime createAt;
 }
