6a37fc9ee7bcecaa36dbee9469b2de0d80cd167f
Seulgi <urina842@gmail.com>
2023-10-04 11:22:00 +0900

    Refactor: 사용하지 않는 코드 삭제

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,31 +1,16 @@
 import { Outlet } from "react-router-dom";
 import SideBar from "./components/SideBar/SideBar";
 import styled, { createGlobalStyle } from "styled-components";
-import { useSelector } from "react-redux";
 import OnBoarding from "pages/Onboarding";
 
 function App() {
-  // const path = window.location.pathname;
-
-  // const notSidebar = path === '/login' || path === '/auth/kakao' || path === '/auth/google' || path === '/profile';
-  // const user = useSelector((state) => state.user);
-
   return (
     <Root>
       <GlobalStyle />
-      {/* {(user.nickname==="" || user.nickname===null || notSidebar)
-      ?
-      <Content>
-        <Outlet />
-        </Content>
-      :
-      <> */}
-      <SideBar />
+        <SideBar />
           <Content>
             <Outlet />
           </Content>
-      {/* </> */}
-      {/* } */}
     </Root>
   );
 }
